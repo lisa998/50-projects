54d2f65e9fd1f2975dabc4e8f0a68225cd293b7f
lisa <xj3zjfan@gmail.com>
2021-11-06 18:45:58 +0800

    fix verify-account-ui

--- src/projects/verifyAccountUI/verifyAccountUI.js
+++ src/projects/verifyAccountUI/verifyAccountUI.js
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import { useState } from "react";
 import { Body, Div, TextDiv, Input } from "./styled";
 
-export default function VerifyAccountUI() {
+export default function VerifyAccountUi() {
   const [value, setValue] = useState(
     new Array(6).fill({ data: "", active: false })
   );
